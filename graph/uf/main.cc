e568bc751e2a531ad68d5620e703a2b1d7e42078
yafei_liu <yafei_liu@foxitsoftware.cn>
2017-10-25 16:47:41 +0800

    algorithm : add union&find method to solve dynamic connectivity problem.

---
+++ graph/uf/main.cc
@@ -0,0 +1,23 @@
+#include "union-find.h"
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+	std::ifstream infile(argv[1]);
+	int size = -1;
+	infile >> size;
+	UnionFind ufclient(size);
+	while(!infile.eof())
+	{
+		int p, q;
+		infile >> p >> q;
+		if (!ufclient.Connected(p, q))
+		{
+			std::cout << p << " " << q << std::endl;
+			ufclient.Union(p, q);
+		}
+	}
+	std::cout << ufclient.Count() << " components" << std::endl;
+	return 0;
+}
