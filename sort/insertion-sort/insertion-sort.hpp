b5312a6edbb8f16d7e7765ec5c393556c9130bce
yafei_liu <yafei_liu@foxitsoftware.cn>
2017-10-26 17:42:30 +0800

    algorithm : add insertion-sort.

---
+++ sort/insertion-sort/insertion-sort.hpp
@@ -0,0 +1,22 @@
+#ifndef IS_INSERTIONSORT_H_
+#define IS_INSERTIONSORT_H_
+#include <vector>
+
+template<typename T>
+class InsertionSort
+{
+public:
+    void Sort(std::vector<T>& t_vec)
+    {
+        for (typename std::vector<T>::iterator it = t_vec.begin() + 1; it != t_vec.end(); ++it)
+        {
+            typename std::vector<T>::iterator temp_it = it;
+            while(temp_it != t_vec.begin() && *temp_it < *std::prev(temp_it))
+            {
+                std::swap(*temp_it, *std::prev(temp_it));
+                std::advance(temp_it, -1);
+            }
+        }
+    }
+};
+#endif
---
+++ sort/main.cc
@@ -0,0 +1,34 @@
+#include "insertion-sort/insertion-sort.hpp"
+#include <random>
+#include <vector>
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+    std::default_random_engine dre(static_cast<unsigned long>(time(0)));
+    std::uniform_int_distribution<int> di(1,50);
+    std::vector<int> sort_array;
+    for (int i = 0; i < 5; ++i)
+    {
+        int value = di(dre);
+        sort_array.push_back(value);
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
+    InsertionSort<int> is_sorter;
+    is_sorter.Sort(sort_array);
+    
+    //for (auto& element : sort_array)
+    //{
+    //  std::cout << element << " ";
+    //}
+    for (std::vector<int>::iterator it = sort_array.begin(); it != sort_array.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
