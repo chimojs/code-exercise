3413a0204fae8cf2ea437dd4ab1e15cfba99a80c
chimojs <1205644371@qq.com>
2017-12-22 16:21:27 +0800

    Update main.cc

--- exercise/JobSchedule/main.cc
+++ exercise/JobSchedule/main.cc
@@ -3,9 +3,9 @@
 
 int main(int argc, char* argv[])
 {
-	std::ifstream fin(argv[1]);
+    std::ifstream fin(argv[1]);
     CPM cpm;
     cpm.Parse(fin);
-	getchar();
-	return 0;
+    getchar();
+    return 0;
 }
